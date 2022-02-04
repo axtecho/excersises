3cdfd7b112d994358e8d5972f19d4f9f67b6a963
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-02-04 16:57:53 +0100

    strugglingwithAppendingProperly

--- calculator_files/index.js
+++ calculator_files/index.js
@@ -2,16 +2,40 @@
 
 const firstnumber = document.querySelector("#firstnumber");
 const secondnumber = document.querySelector("#secondnumber");
-let fn = Number(firstnumber.value);
-let sn = Number(secondnumber.value);
-let finalResult = fn + sn;
 const calculate = document.querySelector("#calculate");
 const result = document.querySelector("#results");
+const operator = document.querySelector("#operator");
+const node = document.createElement("li");
+let finalResult;
+
+let fn = Number(firstnumber.value);
+let sn = Number(secondnumber.value);
+
 calculate.addEventListener("click", reading);
 
 function reading() {
-  result.firstChild.textContent = finalResult;
+  if (operator.value === "sub") {
+    finalResult = fn - sn;
+  } else if (operator.value === "mul") {
+    finalResult = fn * sn;
+
+    /*     console.log(finalResult);
+     */
+  } else if (operator.value === "div") {
+    finalResult = fn / sn;
+    /*     console.log(finalResult);
+     */
+  } else if (operator.value === "add") {
+    finalResult = fn + sn;
+
+    /*     console.log(finalResult);
+     */
+  }
   calculating();
 }
 
-function calculating() {}
+function calculating(opValue) {
+  const displayResult = document.createTextNode(finalResult);
+  node.appendChild(displayResult);
+  result.appendChild(node);
+}
