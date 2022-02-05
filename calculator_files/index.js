d534b1f00ed0736f92f13b55adeeed20657424bf
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-02-05 12:06:38 +0100

    clear result works

--- calculator_files/index.js
+++ calculator_files/index.js
@@ -1,8 +1,7 @@
 "use strict";
-
+window.addEventListener("load", start);
 const firstnumber = document.querySelector("#firstnumber");
 const secondnumber = document.querySelector("#secondnumber");
-const calculate = document.querySelector("#calculate");
 const result = document.querySelector("#results");
 const operator = document.querySelector("#operator");
 let finalResult;
@@ -10,25 +9,21 @@ let finalResult;
 let fn = Number(firstnumber.value);
 let sn = Number(secondnumber.value);
 
-calculate.addEventListener("click", reading);
-
+function start() {
+  document.querySelector("#calculate").addEventListener("click", reading);
+  document.querySelector("#clear").addEventListener("click", clearResults);
+}
 function reading() {
+  let fn = Number(firstnumber.value);
+  let sn = Number(secondnumber.value);
   if (operator.value === "sub") {
     finalResult = fn - sn;
   } else if (operator.value === "mul") {
     finalResult = fn * sn;
-
-    /*     console.log(finalResult);
-     */
   } else if (operator.value === "div") {
     finalResult = fn / sn;
-    /*     console.log(finalResult);
-     */
   } else if (operator.value === "add") {
     finalResult = fn + sn;
-
-    /*     console.log(finalResult);
-     */
   }
   calculating();
 }
@@ -38,4 +33,14 @@ function calculating(opValue) {
   const displayResult = document.createTextNode(finalResult);
   node.appendChild(displayResult);
   result.appendChild(node);
+  reAssignValue();
+}
+
+function reAssignValue() {
+  firstnumber.value = finalResult;
+}
+
+function clearResults() {
+  result.innerHTML = "";
+  console.log(result.childNodes);
 }
