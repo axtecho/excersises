9a19a93885dc328255a23ffd5a78f818299fd77a
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-02-06 20:36:20 +0100

    homework done

--- strings.js
+++ strings.js
@@ -21,20 +21,63 @@ console.log(`${person} is ${len} characters long `);
 
 const wizard = "Albus Percival Wulfric Brian Dumbledore";
 const wizardLength = wizard.length;
-const Q2 = wizard[2];
-const Q3 = wizard[6];
-const Q4 = wizard[29];
-const Q5 = wizard[38];
 
 /* Q1 */
 console.log(`QUESTION ONE = ${wizard.length}`);
 
 /* Q2 */
+const Q2 = wizard[2];
 console.log(`QUESTION TWO = ${Q2}`);
 
 /* Q3 */
+const Q3 = wizard[6];
 console.log(`QUESTION THREE = ${Q3}`);
 /* Q4 */
+const Q4 = wizard[29];
 console.log(`QUESTION FOUR = ${Q4}`);
 /* Q5 */
+const Q5 = wizard[38];
 console.log(`QUESTION FIVE = ${Q5}`);
+
+/* -----------------------Methods----------------------- */
+
+const str1 = "     There is        space here \n";
+const str2 = str1.trim();
+
+console.log(`_${str1}_`);
+console.log(`_${str2}_`);
+
+/* -----------------------Methods parameters----------------------- */
+
+const fullname = "Lasse Weber Joergensen";
+const firstname = fullname.substring(0, 5);
+const lastname = fullname.substring(0);
+
+console.log(`_${firstname}_`);
+console.log(`_${lastname}_`);
+/* -----------------------Substring----------------------- */
+/* const wizard = "Albus Percival Wulfric Brian Dumbledore";
+ */
+/* Q1 */
+const SQ1 = wizard.substring(0, 5);
+console.log(`_${SQ1}_`);
+
+/* Q2 */
+const SQ2 = wizard.substring(29);
+console.log(`_${SQ2}_`);
+
+/* Q3 */
+const SQ3 = wizard.substring(15, 22);
+console.log(`_${SQ3}_`);
+
+/* Q4 */
+const SQ4 = wizard.substring(14, 23);
+console.log(`_${SQ4}_`);
+
+/* Q5 */
+const SQ5 = wizard.substring(25, 28);
+console.log(`_${SQ5}_`);
+
+/* Q6 */
+const SQ6 = wizard.substring(2, 5);
+console.log(`_${SQ6}_`);
