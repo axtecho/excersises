76168bf7ffdad30e8c4ccb648a6a27a25c8895fd
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-02-09 21:16:06 +0100

    significant message

--- bigfatstring.js
+++ bigfatstring.js
@@ -1,32 +1,61 @@
+"use strict";
 window.addEventListener("load", start);
-const input = document.querySelector("#textinput");
 const output = document.querySelector("#output");
 const generate = document.querySelector("#generate");
-
+const options = document.querySelector("#options");
+let info;
 function start() {
   generate.addEventListener("click", determineAction);
 }
 
 function determineAction() {
+  const input = document.querySelector("#textinput").value;
+
+  if (options.value === "opt1") {
+    const changeInput = input.charAt(0).toUpperCase();
+    info = changeInput + input.substring(1).toLowerCase();
+  } else if (options.value === "opt2") {
+    info = input.substring(input.indexOf(1), input.indexOf(" "));
+  } else if (options.value === "opt3") {
+    const firstNameLength = input.substring(
+      input.indexOf(1),
+      input.indexOf(" ")
+    );
+    info = firstNameLength.length;
+  } else if (options.value === "opt4") {
+    const middleNamePos1 = input.indexOf(" ") + 1;
+    const middleNamePos2 = input.lastIndexOf(" ");
+
+    info = input.substring(middleNamePos1, middleNamePos2);
+  } else if (options.value === "opt5") {
+  } else if (options.value === "opt6") {
+    console.log("opt6");
+  } else if (options.value === "opt7") {
+    console.log("opt7");
+  } else if (options.value === "opt8") {
+    console.log("opt8");
+  }
   displayOutput();
 }
 
 function displayOutput() {
-  console.log("heyoo");
+  /*   console.log(toUpperCase + version2.substring(1).toLowerCase());
+   */
+  console.log(info);
 }
 
 const Lasse = "lasse weber joergensen";
 const index = Lasse.indexOf(" ") - 1;
 
 /* Q1
-If input is a first name: Make the first character in a name uppercase, and the rest lowercase
-const toUpperCase = Lasse.charAt(0).toUpperCase();
+If input is a first name: Make the first character in a name uppercase, and the rest lowercase */
+/* const toUpperCase = Lasse.charAt(0).toUpperCase();
 console.log(toUpperCase + Lasse.substring(1).toLowerCase()); */
 
 /* Q2
-If input is a full name: Find the first name
-console.log(`_${Lasse.substring(Lasse.indexOf(1), Lasse.indexOf(" "))}_`);
- */
+If input is a full name: Find the first name */
+/* console.log(`_${Lasse.substring(Lasse.indexOf(1), Lasse.indexOf(" "))}_`); */
+
 /* Q3
 If input is a full name: Find the length of the first name */
 
@@ -39,8 +68,7 @@ If input is a full name: Find the middle name start and end position, and the mi
 /* const middleNamePos1 = Lasse.indexOf(" ") + 1;
 const middleNamePos2 = Lasse.lastIndexOf(" ");
 
-console.log(Lasse.substring(middleNamePos1, middleNamePos2));
- */
+console.log(Lasse.substring(middleNamePos1, middleNamePos2)); */
 
 /* Q5
 If input is a filename: Check if a filename is .png or .jpg */
@@ -61,6 +89,6 @@ console.log("*".repeat(password.length));
 /* Q7
 With any input: Make the third character uppercase */
 
-const thirdUpperCase = Lasse.charAt(3).toUpperCase();
+/* const thirdUpperCase = Lasse.charAt(3).toUpperCase();
 
-console.log(thirdUpperCase.substring(0).toLowerCase());
+console.log(thirdUpperCase.substring(0).toLowerCase()); */
--- countingarray.js
+++ countingarray.js
@@ -4,11 +4,9 @@ const array = [];
 let counter = 0;
 
 function clock() {
-  /*   console.log(counter);
-   */
   counter++;
   if (counter < 9) {
-    setTimeout(clock, 1000);
+    setTimeout(clock, 2000);
   }
 
   array.push(counter);
